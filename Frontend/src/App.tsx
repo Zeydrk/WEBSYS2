42a15a6e15cac5a0c65238abff8564868d5b382a
Zeydrk <zedricveran7@gmail.com>
2025-11-23 16:55:23 +0800

    Created initial products page and navbar

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './output.css';
+import Products from './components/Products';
+
+// components
+import Navbar from './components/Navbar';
 
 function App() {
   return (
-    <></>
+    <>
+      <Navbar />
+      <Products />
+    </>
   );
 }
 
--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,21 +1,23 @@
-// components 
+import React from 'react';
+import logo from '../imgs/istc-logo.png'
 
-// main code
-export default function Navbar(){
-    return(
-        <>
-            <header>
-                <img src="" alt="" />
-                <h1></h1>
-                <nav>
-                    <ul>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
-                </nav>
-            </header>
-        </>
-    )
+export default function Navbar() {
+  return (
+    <nav className="navbar bg-base-100">
+      <div className="navbar-start px-5">
+        <img src={logo} alt="logo.png" className='size-8 mr-2'/>
+        <h1>PROJECT:ISTC</h1>
+      </div>
+      <div className="navbar-end">
+        <ul className="menu menu-horizontal px-5 gap-1.5">
+            <li><button>Home</button></li>
+            <li><button>Profile</button></li>
+            <li><button>Shop</button></li>
+            <li><button>Orders</button></li>
+            <li><button>Contact Us</button></li>
+            <li><button>Cart</button></li>
+        </ul>
+      </div>
+    </nav>
+  );
 }
