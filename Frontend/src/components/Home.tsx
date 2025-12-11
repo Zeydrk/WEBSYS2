ad65c360c6e1a49f80365e6c3b594e23848062da
Zeydrk <zedricveran7@gmail.com>
2025-12-12 01:51:31 +0800

    preparing to merge with main

--- Frontend/src/components/Home.tsx
+++ Frontend/src/components/Home.tsx
@@ -1,7 +1,13 @@
 // imports
 import backImage from "../imgs/pexels-pixabay-2150.jpg"
+import { useNavigate } from "react-router-dom";
 
 export default function Home() {
+  const Navigate = useNavigate()
+
+  function shopLink(){
+      Navigate('/products')
+  }
   return (
     <>
       <div className="min-h-screen bg-linear-to-b from-slate-900 via-purple-900 to-slate-900 text-gray-100 font-sans">
@@ -27,7 +33,7 @@ export default function Home() {
             </div>
             <div>
               {/* UPDATED BUTTON HERE */}
-              <button className="btn btn-primary btn-lg border-none bg-linear-to-r from-purple-600 to-pink-600 text-white shadow-lg shadow-purple-500/50 hover:scale-105 transition-transform">
+              <button onClick={shopLink} className="btn btn-primary btn-lg border-none bg-linear-to-r from-purple-600 to-pink-600 text-white shadow-lg shadow-purple-500/50 hover:scale-105 transition-transform">
                 Shop Now
               </button>
             </div>
